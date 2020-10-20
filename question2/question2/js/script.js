50b2bf772abf198de8bd16db6ab51f39a22130e4
tomerikw <61654560+tomerikw@users.noreply.github.com>
2020-10-20 23:54:27 +0200

    Add files via upload

--- question2/question2/js/script.js
+++ question2/question2/js/script.js
@@ -8,12 +8,17 @@ async function fetchGames() {
   const json = await response.json();
   console.log(json);
 
+  container.innerHTML = "";
+
   const results = json.results;
 
   results.forEach(function (games) {
-    container.innerHTML += `<h1>${games.name}</h1>
+    container.innerHTML += `<a href="question3.html?id=${games.id}">
+                            <h1>${games.name}</h1>
+                            <div class="image" style="background-image: url(${games.background_image});"></div>
                             <div>${games.rating}</div>
-                            <div>${games.released}</div>`;
+                            <div>${games.released}</div>
+                            </a>`;
   });
 }
 
